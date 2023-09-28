54490caeca49eb3375fb9cc0d4ae7222310b7238
Sreekanth K <srekthk@gmail.com>
2023-09-28 11:50:12 +0530

    Interface improvements

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import LandingPage from './Pages/LandingPage';
+import WatchHistory from './Pages/WatchHistory';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div>
-      <p>Test</p>
+      <Header />
       <Routes>
         <Route path = '/' element = {<LandingPage />}/>
         <Route path='/home' element = {<Home />} />
+        <Route path='/watch-history' element={<WatchHistory />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Navbar, Container} from 'react-bootstrap'
+
+function Header() {
+    return (
+        <Navbar className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                    alt=""
+                    src="/img/logo.svg"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    />{' '}
+                    React Bootstrap
+                </Navbar.Brand>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header
